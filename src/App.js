aacef8b1e772975a274bd68182c6f7c50c1b9964
joao <joaob.linonunes@gmail.com>
2021-07-09 18:25:14 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Teste from './Componentes/Teste';
+
+function App() {
+  return (
+    <Teste />
+  );
+}
+
+export default App;
---
+++ src/Componentes/Teste.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+                <img src = "https://indoittraining.com/wp-content/uploads/sites/3/2020/11/reactjs.png" alt="exibirImagem"
+                title="imagem" width="20%" height="auto"></img> <br></br>
+                <h1> Primeiro componente !!</h1>
+            </div>
+        );
+    }
+}
+
+export default Teste;
